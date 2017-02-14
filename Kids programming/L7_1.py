2ec62a372244dd425ad3d89ca06c52d79482b268
JKChang2015 <jkchang2015@gmail.com>
2017-02-14 22:55:23 +0000

    update

---
+++ Kids programming/L7_1.py
@@ -0,0 +1,8 @@
+num1 = float(raw_input('enter the first number '))
+num2 = float(raw_input('enter the second number '))
+if num1 > num2:
+    print num1, 'is larger than', num2
+if num1 < num2:
+    print num1, 'is smaller than',num2
+if num1 == num2:
+    print num1, 'is equal to', num2
---
+++ Kids programming/c7.py
@@ -0,0 +1,5 @@
+age = int(raw_input('pls input your age: '))
+if not (age > 18):
+    print 'you are not allow to buy alcohol'
+else:
+    print 'you can buy alcohol'
