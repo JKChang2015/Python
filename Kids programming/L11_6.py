7b168e9a40a5c63fb190c68187fdb598fa6a31dc
JKChang2015 <jkchang2015@gmail.com>
2017-02-15 16:21:36 +0000

    update

---
+++ Kids programming/L11_6.py
@@ -0,0 +1,12 @@
+# different combinations of hot dog making
+
+print ' \t\tDog \tBun \tKetchup \tMustard \tOnions'
+count = 1
+for dog in [0 , 1]:
+    for bun in [0,1]:
+        for ketchup in [0,1]:
+            for mustard in [0,1]:
+                for onions in [0,1]:
+                    print '#', count, '\t',
+                    print dog, "\t\t", bun, "\t\t", ketchup, "\t\t\t", mustard, '\t\t\t', onions
+                    count +=1
