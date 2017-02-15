69058e587c541971789322edeb7a1ce2c2ea4d15
JKChang2015 <jkchang2015@gmail.com>
2017-02-15 11:35:43 +0000

    skier

--- Kids programming/L8_8.py
+++ Kids programming/L8_8.py
@@ -1,8 +1,8 @@
 
-someinput = raw_input('pls input a number between 0 to 10: ')
+someinput = raw_input('pls input a number between 0 and 10: ')
 
 while (int(someinput) >= 0 and int(someinput) <= 10):
-    print 'thank you for your input, your input is legal \n'
-    print 'please input a numer between 0 to 10'
-    someinput = raw_input()
+    print someinput , 'is legal \n'
+    someinput = raw_input('please input a numer and 0 to 10:')
+
 print 'Sorry, your input is out of range, quite the program'
---
+++ Kids programming/c8.py
@@ -0,0 +1,4 @@
+input = int(raw_input('pls input a number: '))
+print 'here is your table: '
+for i in range (1, 10+1):
+    print input, 'x', i ,'= ', input*i
