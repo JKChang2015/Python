817e7fe1575b1c1ecd6b3e9d9668c8a2a34d3e95
JKChang2015 <jkchang2015@gmail.com>
2017-02-16 20:09:46 +0000

    update

--- Kids programming/L12_sort.py
+++ Kids programming/L12_sort.py
@@ -1,10 +1,24 @@
 origin = [ 'Tom', 'Amy', 'David', 'Matt', 'Dave']
-newSort1 = origin[:]
-newSort2 = origin[:]
 
-newSort1.sort()
-newSort2.sort(reverse=True)
+sort1 = sorted(origin)
+sort2 = sorted(origin)
+sort2.reverse()
+
 
 print 'origin: ', origin
-print 'sorted: ', newSort1
-print 'reverse sorted: ', newSort2
+print 'sorted: ', sort1
+print 'reverse sorted: ', sort2
+
+
+
+# newSort1 = origin[:]
+# newSort2 = origin[:]
+#
+# newSort1.sort()
+# newSort2.sort(reverse=True)
+#
+# print 'origin: ', origin
+# print 'sorted: ', newSort1
+# print 'reverse sorted: ',
+
+
--- Kids programming/tr.py
+++ Kids programming/tr.py
@@ -1,11 +1,34 @@
 import copy
 
-origin = ('a', 'in', 'Java')
-cpy = copy.deepcopy(origin)
-print 'origin is cpy: ',origin is cpy
+origin = ['a', 2, 'Java']
+cpy = copy.copy(origin)
 
-origin = ('a', 8, 'Java', [1,2,3,4])
-cpy = copy.deepcopy(origin)
-print 'origin is cpy: ',origin is cpy
+print 'id of origin:', id(origin)
+for ele in origin:
+    print '[', ele, ',', id(ele), ']',
+
+print '\n'
+
+print 'id of cpy', id(cpy)
+for ele in cpy:
+    print '[', ele, ',', id(ele), ']',
+
+print '\n\n origin is cpy: ',origin is cpy
+
+origin[1] = 1112
+print origin
+print cpy
+
+
+
+
+
+#
+#
+# print 'origin is cpy: ',origin is cpy
+#
+# origin = ('a', 8, 'Java', [1,2,3,4])
+# cpy = copy.deepcopy(origin)
+# print 'origin is cpy: ',origin is cpy
 
 
