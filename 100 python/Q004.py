c39c2d979ebdc3276e0755f040b2b85c03525436
JKChang2015 <jkchang2015@gmail.com>
2017-02-17 10:09:07 +0000

    update

--- 100 python/Q004.py
+++ 100 python/Q004.py
@@ -1,3 +1,18 @@
+# Q004
 # Created by JKChang
 # 16/02/2017, 21:22
-# Description:
+# Description: Write a program which accepts a sequence of comma-separated numbers from console and generate a list
+#              and a tuple which contains every number.
+#
+# Suppose the following input is supplied to the program:
+# 34,67,55,33,12,98
+# Then, the output should be:
+# ['34', '67', '55', '33', '12', '98']
+# ('34', '67', '55', '33', '12', '98')
+
+seq = '12,23,45,68,21,43,87'
+list = seq.split(',')
+tuple = tuple(list)
+
+print list
+print tuple
--- 100 python/Q005.py
+++ 100 python/Q005.py
@@ -1,5 +1,6 @@
 # Q005
 # Created by JKChang
 # 17/02/2017, 09:00
-# Description: 
-
+# # Description: Define a class which has at least two methods:
+#                getString: to get a string from console input # printString: to print the string in upper case.
+#                Also please include simple test function to test the class methods.
