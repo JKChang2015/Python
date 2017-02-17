d44de65895386d30e3b1a6bb40aee823b668f708
JKChang2015 <jkchang2015@gmail.com>
2017-02-17 09:45:28 +0000

    Q002

--- 100 python/Q002.py
+++ 100 python/Q002.py
@@ -1,3 +1,15 @@
+# Q002
 # Created by JKChang
 # 16/02/2017, 21:16
-# Description:
+# Description: Write a program which can compute the factorial of a given numbers.
+#              The results should be printed in a comma-separated sequence on a single line.
+#              Suppose the following input is supplied to the program:
+
+def fact(x):
+    if x == 0:
+        return 1;
+    return x * fact(x - 1)
+
+
+x = int(raw_input('pls input a number: '))
+print fact(x)
