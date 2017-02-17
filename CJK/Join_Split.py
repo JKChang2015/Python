612f6e171b0416662d5e91a487a79ffbf99fff7f
JKChang2015 <jkchang2015@gmail.com>
2017-02-17 09:28:45 +0000

    Join & Split

---
+++ CJK/Join_Split.py
@@ -0,0 +1,14 @@
+# Join_Split
+# Created by JKChang
+# 17/02/2017, 09:17
+# Description: test join and Split
+
+list = ['a', 'b', 'c']
+print '-'.join(list)
+
+seq = 'a-b-c-d-e'
+l = seq.split()
+print l
+
+l2 = seq.split('-')
+print l2
