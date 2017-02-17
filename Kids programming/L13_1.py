e341debcc86cbe18386685ea0fe9a17763c4877f
JKChang2015 <jkchang2015@gmail.com>
2017-02-17 12:13:08 +0000

    Function

---
+++ Kids programming/L13_1.py
@@ -0,0 +1,14 @@
+# L13_1
+# Created by JKChang
+# 17/02/2017, 11:07
+# Description:  create a function
+
+def printMyAddress():
+    print 'David C'
+    print '7 St. Marks Road'
+    print 'Preston'
+    print 'PR18TL'
+    print
+
+
+printMyAddress()
---
+++ Kids programming/L13_2.py
@@ -0,0 +1,22 @@
+# L13_2
+# Created by JKChang
+# 17/02/2017, 11:40
+# Description: function with argument
+
+def printMyAddress(myName):
+    print myName
+    print '7 St. Marks Road'
+    print 'Preston'
+    print 'PR18TL'
+    print
+
+
+def printMyAddress2(myName, psCode):
+    print myName
+    print '7 St. Marks Road'
+    print 'Preston'
+    print psCode
+    print
+
+
+printMyAddress('David')
