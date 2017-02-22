e1b5949029a086004e336da9e9b4b7e73049e03a
JKChang2015 <jkchang2015@gmail.com>
2017-02-22 14:49:50 +0000

    update

---
+++ Python programming for Beginers/L14_Inheritance.py
@@ -0,0 +1,4 @@
+# L14_Inheritance
+# Created by JKChang
+# 22/02/2017, 14:41
+# Description:
---
+++ Python programming for Beginers/L14_polymorphism.py
@@ -0,0 +1,29 @@
+# L14_polymorphism
+# Created by JKChang
+# 22/02/2017, 14:10
+# Description: a polymorphism example
+
+class Triangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def getArea(self):
+        area = self.width * self.height / 2.0
+        return area
+
+
+class Square:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def getArea(self):
+        area = float(self.width * self.height)
+        return area
+
+
+tra = Triangle(6, 6)
+print tra.getArea()
+sqr = Square(6, 6)
+print sqr.getArea()
