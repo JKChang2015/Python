590471d62f4561b384411cb80420ebe894d9aace
JKChang2015 <jkchang2015@gmail.com>
2017-02-22 13:13:11 +0000

    m

--- 100 python/Q005.py
+++ 100 python/Q005.py
@@ -5,3 +5,16 @@
 #                getString: to get a string from console input # printString: to print the string in upper case.
 #                Also please include simple test function to test the class methods.
 
+class InputOutputString(object):
+    def __int__(self):
+        self.s = ''
+
+    def getString(self):
+        self.s = raw_input('input: ')
+
+    def printString(self):
+        print self.s.upper()
+
+test = InputOutputString()
+test.getString()
+test.printString()
