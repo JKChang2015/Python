189825e5b270bbb2f7f5e237493282c9c47418a3
JKChang2015 <jkchang2015@gmail.com>
2017-02-23 14:37:03 +0000

    Q008

---
+++ 100 python/Q008.py
@@ -0,0 +1,15 @@
+# Q008
+# Created by JKChang
+# 23/02/2017, 14:28
+# Description: Write a program that accepts a comma separated sequence of words as input and prints the words in a
+#              comma-separated sequence after sorting them alphabetically.
+#
+# Example: Suppose the following input is supplied to the program:
+#          without,hello,bag,world
+#          Then, the output should be:
+#          bag,hello,without,world
+
+
+terms = [term.strip(" ") for term in raw_input().split(',')]
+terms.sort()
+print(",".join(terms))
