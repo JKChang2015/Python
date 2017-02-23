290c15ae77b65e8f29a9194048af8d5201af826e
JKChang2015 <jkchang2015@gmail.com>
2017-02-23 14:27:48 +0000

    L14

--- Python programming for Beginers/L14_Inheritance.py
+++ Python programming for Beginers/L14_Inheritance.py
@@ -1,4 +1,34 @@
 # L14_Inheritance
 # Created by JKChang
 # 22/02/2017, 14:41
-# Description:
+# Description: inherit example
+
+class Parent:
+    __parentAttr = 100
+
+    def __init__(self):
+        print "parent's construction method"
+
+    def prentMethod(self):
+        print "parent's method"
+
+    def getAttr(self):
+        print "parent's attribute: ", self.parentAttr
+
+    def setAttr(self, attr):
+        self.parentAttr = attr
+
+
+class Child(Parent):
+    def __init__(self):
+        print "Child's construction method"
+
+    def childMethod(self):
+        print "Child's method"
+
+
+c = Child()
+c.childMethod()
+c.prentMethod()
+c.setAttr(200)
+c.getAttr()
