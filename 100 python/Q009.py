d7b9324c216a9c5f6c62ba1f08d1eee1aed0090c
JKChang2015 <jkchang2015@gmail.com>
2017-02-23 14:44:36 +0000

    Q009

---
+++ 100 python/Q009.py
@@ -0,0 +1,23 @@
+# Q009
+# Created by JKChang
+# 23/02/2017, 14:39
+# Description: Write a program that accepts sequence of lines as input and prints the lines after making all
+#              characters in the sentence capitalized.
+#
+# Example: Suppose the following input is supplied to the program:
+# Hello world
+# Practice makes perfect
+# Then, the output should be:
+# HELLO WORLD
+# PRACTICE MAKES PERFECT
+
+lines = []
+while True:
+    s = raw_input()
+    if s:
+        lines.append(s.upper())
+    else:
+        break
+
+for an in lines:
+    print an
