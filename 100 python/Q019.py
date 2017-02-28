841ccd10084f5d5522cad6e35a67ec7e46c4f9e6
JKChang2015 <jkchang2015@gmail.com>
2017-02-28 16:54:55 +0000

    Q019

--- 100 python/Q019.py
+++ 100 python/Q019.py
@@ -17,5 +17,13 @@
 # Then, the output of the program should be:
 # [('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21', '85'), ('Tom', '19', '80')]
 
+from operator import itemgetter, attrgetter
 
+l = []
+while True:
+    s = raw_input()
+    if not s:
+        break
+    l.append(tuple(s.split(",")))
 
+print sorted(l, key=itemgetter(0, 1, 2))
