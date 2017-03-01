a407ee2ffaa0960bab699b4106a52bb6ef839ea3
JKChang2015 <jkchang2015@gmail.com>
2017-03-01 11:29:16 +0000

    Fibonacci sequence

---
+++ Runood_100/R006.py
@@ -0,0 +1,37 @@
+# R006
+# Created by JKChang
+# 01/03/2017, 11:13
+# Description: Fibonacci sequence
+
+# -------------------Method 1---------------------
+def fib(n):
+    a, b = 1, 1
+    for i in range(n - 1):
+        a, b = b, a + b
+    return a
+
+
+print fib(10)
+
+
+# -------------------Method 2---------------------
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+
+print fib(10)
+
+def pfib(n):
+    if n ==1:
+        return [1]
+    if n == 2:
+        return [1,1]
+
+    fibs = [ 1,1 ]
+    for i in range(2, n):
+        fibs.append(fibs[-1] + fibs[-2])
+    return fibs
+
+print pfib(10)
