a7e846579bb5a430c7f1d6b99f7cdc9df16e6293
JKChang2015 <jkchang2015@gmail.com>
2017-03-01 11:05:10 +0000

    R004

--- Runood_100/R004.py
+++ Runood_100/R004.py
@@ -3,17 +3,44 @@
 # 01/03/2017, 10:31
 # Description: Count the days for a certain date.
 
-leap = False
+def monthDay(month):
+    swithcher = {
+        1: 31,
+        2: 28,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31
+    }
+    return swithcher.get(month)
 
-year = raw_input('pls input year')
+
+res = 0
+
+leap = False
+year = int(raw_input('pls input year: '), )
 if year < 0:
     print 'illegal year'
 if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
     leap = True
 
-month = raw_input('pls input month')
-if 0 < month <= 12:
+month = int(raw_input('pls input month: '), )
+if (month < 0) or (month > 12):
     print 'illegal month'
 
-day = raw_input('pls input the day')
+for mon in range(1, month):
+    res += monthDay(mon)
 
+day = int(raw_input('pls input the day: '), )
+res += day
+
+if (month > 2) and leap:
+    res += 1
+
+print 'it is the %dth day.' % res
