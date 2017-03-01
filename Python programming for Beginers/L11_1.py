d4ccfb9fa85ba8db7afdcea14b701f41ed77323a
JKChang2015 <jkchang2015@gmail.com>
2017-03-01 11:40:28 +0000

    R007 and R008

--- Python programming for Beginers/L11_1.py
+++ Python programming for Beginers/L11_1.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 # multiplication table
-for i in range (1, 10):
-    for j in range ( 1, 10):
+for i in range(1, 10):
+    for j in range(1, 10):
         if (i >= j):
-            print j, "x", i, "=", i*j, '\t',
+            print j, "x", i, "=", i * j, '\t',
     print
-
-
