8f79ccd6b42f2af6d0bc80908be8a1e6b255843d
JKChang2015 <jkchang2015@gmail.com>
2017-03-01 11:12:48 +0000

    R005

---
+++ Runood_100/R005.py
@@ -0,0 +1,10 @@
+# R005
+# Created by JKChang
+# 01/03/2017, 11:05
+# Description: ranking three input integers
+
+l = [int(x) for x in raw_input('pls input three integer: ').split(' ')]
+l.sort()
+print l
+
+
