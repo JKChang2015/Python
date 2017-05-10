a8f5b5da2ba6ea1064f6fe5635fec37191bb8c0e
JKChang2015 <jkchang2015@gmail.com>
2017-05-10 16:55:32 +0100

    update

---
+++ CJK/ReadMeCol.py
@@ -0,0 +1,15 @@
+# ReadMeCol
+# Created by JKChang
+# 10/05/2017, 16:38
+# Description: every column of the readme file
+
+from enum import Enum
+
+
+class ReadMeCol(object):
+
+
+
+
+    def __init__(self):
+        pass
