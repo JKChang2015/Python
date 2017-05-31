78215ed5b339f139451515f13798b2ce42da28f8
JKChang2015 <jkchang2015@gmail.com>
2017-05-31 21:49:03 +0100

    Q

--- w3resource/basic/Q013.py
+++ w3resource/basic/Q013.py
@@ -2,5 +2,17 @@
 # Q013
 # Created by JKChang
 # Wed, 31/05/2017, 15:34
-# Tag: 
+# Tag: print multiple line
 # Description: Write a Python program to print the following here document. 
+# Sample string :
+# a string that you "don't" have to escape
+# This
+# is a ....... multi-line
+# heredoc string --------> example
+
+print """
+a string that you "don't" have to escape
+This
+is a  ....... multi-line
+heredoc string --------> example
+"""
---
+++ w3resource/basic/Q015.py
@@ -0,0 +1,16 @@
+# Q015
+# Created by JKChang
+# 31/05/2017, 21:44
+# Tag:
+# Description: Write a Python program to get the volume of a sphere with radius 6.
+# V = 4/3 * pi * (r**3)
+
+from math import pi
+
+
+def volume(radius):
+    return 4 / 3 * pi * (radius ** 3)
+
+
+r = 6
+print volume(6)
