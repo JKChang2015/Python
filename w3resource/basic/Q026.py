3083388a7194d7fee6d84f3881a393d8a1bb14cb
JKChang2015 <jkchang2015@gmail.com>
2017-05-31 22:52:19 +0100

    update

--- w3resource/basic/Q026.py
+++ w3resource/basic/Q026.py
@@ -2,5 +2,20 @@
 # Q026
 # Created by JKChang
 # Wed, 31/05/2017, 15:34
-# Tag: 
+# Tag: histogram
 # Description: Write a Python program to create a histogram from a given list of integers. 
+
+def histogram(l):
+    top = max(l)
+    while top > 0:
+        for i in l:
+            if i - top >= 0:
+                print '$',
+            else:
+                print ' ',
+        print
+        top = top - 1
+
+
+li = [1, 2, 7, 6]
+histogram(li)
