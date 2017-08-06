63a894108ceea76898adb7adb5df967b68b106ac
JKChang2015 <jkchang2015@gmail.com>
2017-08-06 16:18:59 +0100

    u

---
+++ CJK/date.py
@@ -0,0 +1,15 @@
+# date
+# Created by JKChang
+# 06/08/2017, 15:53
+# Tag:
+# Description: 
+
+import datetime
+
+start_date = datetime.date(2017,3,4)
+print(start_date)
+
+for i in range(20):
+    start_date = start_date + datetime.timedelta(28)
+    print(start_date)
+    i+=i
