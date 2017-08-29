fa0c076dc012f4517e2e4dcbda5bf34e68b25c49
JKChang2015 <jkchang2015@gmail.com>
2017-08-29 11:59:34 +0100

    update

--- CJK/clean.py
+++ CJK/clean.py
@@ -7,28 +7,47 @@
 # load the text file
 # clean the file
 
+import string
+
 
 def question(path):
     with open(path) as f:
-        content = f.readlines()
+
+        content = f.read()
+        s = content
+        lines = content.split('\n')
+        serial = []
+        for line in lines:
+            translator = str.maketrans('', '', string.punctuation)
+            line = line.translate(translator)
+            if len(line) != 0 and line != '\n':
+                value = line.split()[0]
+                if value.isdigit():
+                    serial.append(value)
+        print(serial)
+
         res = []
-        for line in content:
-            line = line.replace('Go to the editor', '')
-            line = line.replace('Click me to see the solution', '')
-            if len(line) == 0 or line == '\n':
-                continue
-            res.append(line)
-
-        for index, elem in enumerate(res):
-
-            if not elem[0].isdigit():
-                res[index - 1:index] = [''.join(res[index - 1:index + 1])]
-                res.remove(index)
-
-        print('\n'.join(res))
+        for ser in serial[1:]:
+            res.append(s.split(ser, 1)[0])
+            s = ser + s.split(ser, 1)[1]
+        res.append(s)
+        print('>'.join(res))
+        return res
 
 
-path = '/Users/jkchang/Downloads/re.txt'
+
+        # res = []
+        #
+        # for line in content:
+        #     line = line.replace('Go to the editor', '')
+        #     line = line.replace('Click me to see the solution', '')
+        #     if len(line) == 0 or line == '\n':
+        #         continue
+        #     res.append(line)
+        #
+
+
+path = '/Users/jkchang/Downloads/ree.txt'
 question(path)
 
 
