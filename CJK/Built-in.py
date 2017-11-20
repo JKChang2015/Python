70254bf3becef510bcb3ed081a93317323149230
JKChang2015 <jkchang2015@gmail.com>
2017-11-20 12:00:13 +0000

    update

--- CJK/Built-in.py
+++ CJK/Built-in.py
@@ -6,21 +6,15 @@
 # Description: Built-in Functions — Python 3.4.7 documentation
 # The Python interpreter has a number of functions and types built into it that are always available.
 
-# Built-in Functions
-# abs()	dict()	help()	min()	setattr()
-# all()	dir()	hex()	next()	slice()
-# any()	divmod()	id()	object()	sorted()
-# ascii()	enumerate()	input()	oct()	staticmethod()
-# bin()	eval()	int()	open()	str()
-# bool()	exec()	isinstance()	ord()	sum()
-# bytearray()	filter()	issubclass()	pow()	super()
-# bytes()	float()	iter()	print()	tuple()
-# callable()	format()	len()	property()	type()
-# chr()	frozenset()	list()	range()	vars()
-# classmethod()	getattr()	locals()	repr()	zip()
-# compile()	globals()	map()	reversed()	__import__()
-# complex()	hasattr()	max()	round()
-# delattr()	hash()	memoryview()	set()
+# __import__()	abs()	all()	any()	ascii()	bin()	bool()	bytearray()
+# bytes()	callable()	chr()	classmethod()	compile()	complex()	delattr()	dict()
+# dir()	divmod()	enumerate()	eval()	exec()	filter()	float()	format()
+# frozenset()	getattr()	globals()	hasattr()	hash()	help()	hex()	id()
+# input()	int()	isinstance()	issubclass()	iter()	len()	list()	locals()
+# map()	max()	memoryview()	min()	next()	object()	oct()	open()
+# ord()	pow()	print()	property()	range()	repr()	reversed()	round()
+# set()	setattr()	slice()	sorted()	staticmethod()	str()	sum()	super()
+# tuple()	type()	vars()	zip()
 
 # --------------------------------------------------------------------------------------------------------------
 # abs(x)
