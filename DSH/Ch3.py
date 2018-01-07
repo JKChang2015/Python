04ef44ca3213f4aa9eb210e71a45b5710b72b806
JKChang2015 <jkchang2015@gmail.com>
2018-01-07 14:23:04 +0000

    ch2 & ch3

---
+++ DSH/Ch3.py
@@ -0,0 +1,32 @@
+# Ch3
+# Created by JKChang
+# 07/01/2018, 12:26
+# Tag:
+# Description: List
+
+l = ['Munich', 'Casablanca', 'Altiplano', 'Tokyo', 'Seattle']
+print('original list: ' + '  '.join(l))
+
+# ------------sorted---------------------------------------
+sort_l = sorted(l)
+print()
+print('original list:\t\t' + '  '.join(l))
+print('sorted list:\t\t' + '  '.join(sort_l))
+
+# ------------sorted(reverse =True)------------------------
+r_sort_l = sorted(l, reverse=True)
+print()
+print('original list:\t\t' + '  '.join(l))
+print('r_sorted list:\t\t' + '  '.join(r_sort_l))
+
+# ------------list.sort()----------------------------------
+l.sort()
+print()
+print('sorted list:\t\t' + '  '.join(l))
+
+# ------------list.sort(reverse=True)----------------------
+l.sort()
+print()
+print('sorted list:\t\t' + '  '.join(l))
+
+print('\nlength of the list is', len(l))
---
+++ DSH/Ch4.py
@@ -0,0 +1,54 @@
+# Ch4
+# Created by JKChang
+# 07/01/2018, 13:20
+# Tag:
+# Description: List operation
+
+# ----------------FOR loop ---------------------
+pizzas = ['pizza1', 'pizza2', 'pizza3']
+for pizza in pizzas:
+    print(pizza)
+
+print()
+animals = ['Elephant', 'Kangaroo', 'Giraffe']
+for x in animals:
+    print(x, 'is a good pet')
+print('Any of them are good')
+
+# ------------------range()----------------------
+print()
+for x in range(1, 21):
+    print(x, end=' ')
+
+# print()
+# for x in range(1, 1000001):  # print 1- 1000000
+#     print(x)
+
+print('\n')
+l = [x for x in range(1, 101)]
+print(sum(l))
+
+# odd number
+print('\nodd number from 1 to 20: ')
+odd_num = [x for x in range(1, 21) if x % 2 != 0]
+for num in odd_num:
+    print(num, end=' ')
+
+# multiple
+print('\nmultiple value of 3 in range 3-30: ')
+three = [x for x in range(3, 31) if x % 3 == 0]
+for num in three:
+    print(num, end=' ')
+
+# power
+print('\ncube value in 1 to 10: ')
+pw = [x ** 3 for x in range(1, 11)]
+for num in pw:
+    print(num, end=' ')
+
+# Cut'the first three items of the list is: ' + ','.join(l[:3]))
+print('\n')
+l = [str(x) for x in range(1,10)]
+print('the first three items of the list is: ' + ','.join(l[:3]))
+print('the middle items of the list is: ' + ','.join(l[len(l)//2-1:len(l)//2+2]))
+print('the last three items of the list is: ' + ','.join(l[-3:]))
