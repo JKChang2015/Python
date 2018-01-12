1aa25713f1039bb74a642b3f44e1ffb45779643c
JKChang2015 <jkchang2015@gmail.com>
2018-01-12 11:19:43 +0000

    migrant

--- PPC/Learning_Log/learning_logs/admin.py
+++ PPC/Learning_Log/learning_logs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from PPC.Learning_Log.learning_logs.models import Topic
+
 # Register your models here.
+admin.site.register(Topic)
--- PPC/Learning_Log/learning_logs/models.py
+++ PPC/Learning_Log/learning_logs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Topic(models.Model):
+    ''''User's learning Topics'''
+    text = models.CharField(max_length=200)  # max_length of the contents
+    date_added = models.DateField(auto_now_add=True)  # auto-set current time
+
+    def __str__(self):
+        ''''return the text field'''
+        return self.text
