3aeec90eebbc068b592659d8f49d24607a1a1436
JKChang2015 <jkchang2015@gmail.com>
2018-01-22 14:30:59 +0000

    random walk

--- PPC/Pro2/rw_visual.py
+++ PPC/Pro2/rw_visual.py
@@ -8,29 +8,26 @@ import matplotlib.pyplot as plt
 
 from PPC.Pro2.random_walk import RandomWalk
 
-# rw = RandomWalk(100)
-# rw.fill_walk()
-#
-# plt.scatter(rw.x_values, rw.y_values, s=5)
-# plt.show()
+# while True:
+rw = RandomWalk(5000)
+rw.fill_walk()
 
-while True:
-    rw = RandomWalk(3000)
-    rw.fill_walk()
+plt.figure(dpi = 128,figsize=(10,6))
 
-    color = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=color, cmap=plt.cm.Oranges, edgecolors='none', s=10)
-    # highlight start/ end POINTS
+color = list(range(rw.num_points))
+plt.scatter(rw.x_values, rw.y_values, c=color, cmap=plt.cm.Oranges, edgecolors='none', s=10)
+# highlight start/ end POINTS
 
-    plt.scatter(0, 0, c='green', edgecolors='none', s=30)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='blue', edgecolors='none', s=30)
+plt.scatter(0, 0, c='green', edgecolors='none', s=30)
+plt.scatter(rw.x_values[-1], rw.y_values[-1], c='blue', edgecolors='none', s=30)
 
-    # hide the axes
-    plt.axes().get_xaxis().set_visible(False)
-    plt.axes().get_yaxis().set_visible(False)
+# hide the axes
+# plt.axes().get_xaxis().set_visible(False)
+# plt.axes().get_yaxis().set_visible(False)
 
-    plt.show()
+plt.savefig('ramdom_walk.png', bbox_inches='tight')
+plt.show()
 
-    keep_running = input('Make another walk? (y/n): ')
-    if keep_running == 'n' or keep_running == 'N':
-        break
+# keep_running = input('Make another walk? (y/n): ')
+# if keep_running == 'n' or keep_running == 'N':
+#     break
