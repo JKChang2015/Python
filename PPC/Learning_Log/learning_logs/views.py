28174a8887d4f4658e6b3b2ed139fbd4cd19a186
JKChang2015 <jkchang2015@gmail.com>
2018-01-22 12:37:54 +0000

    merge not sucessful

--- PPC/Learning_Log/learning_logs/views.py
+++ PPC/Learning_Log/learning_logs/views.py
@@ -2,13 +2,10 @@
 
 from django.shortcuts import render
 
-from .models import Topic
-
 
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
-
 
 # def topics(request):
 #     """Show all topics."""
