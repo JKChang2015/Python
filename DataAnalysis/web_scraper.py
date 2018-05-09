bad838d7b0144251308778dd3dbf1ce059b19bbe
JKChang2015 <jkchang2015@gmail.com>
2018-05-09 20:47:58 +0100

    upadte

--- DataAnalysis/web_scraper.py
+++ DataAnalysis/web_scraper.py
@@ -3,5 +3,23 @@
 # 07/03/2018, 12:28
 # Tag:
 # Description: 
-# https://www.dataquest.io/blog/web-scraping-tutorial-python/
+# http://www.compjour.org/warmups/govt-text-releases/collect-lists-of-obama-press-briefings/
 
+import requests
+from os import makedirs
+import os
+
+URL_ENDPOINT = 'https://www.whitehouse.gov/briefings-statements/page/'
+MAX_PAGE_NUM = 30
+INDEX_PAGES_DIR = 'index-pages'
+
+makedirs(INDEX_PAGES_DIR,exist_ok=True)
+
+for pagenum in range(MAX_PAGE_NUM):
+    resp = requests.get(os.path.join(URL_ENDPOINT +''.format(pagenum)))
+    print('Downloading',resp.url)
+
+    fname = os.path.join(INDEX_PAGES_DIR,'{}.html'.format(pagenum))
+    print('Saving',fname)
+    with open(fname,'w') as wf:
+        wf.write(resp.text)
