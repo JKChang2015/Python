4f75a37b9c72a4421d95d3fe28e5fde218e8ffe1
JKChang2015 <jkchang2015@gmail.com>
2018-05-10 11:45:13 +0100

    spyder

--- DataAnalysis/web_scraper.py
+++ DataAnalysis/web_scraper.py
@@ -10,13 +10,13 @@ from os import makedirs
 import os
 
 URL_ENDPOINT = 'https://www.whitehouse.gov/briefings-statements/page/'
-MAX_PAGE_NUM = 30
+MAX_PAGE_NUM = 261 #259
 INDEX_PAGES_DIR = 'index-pages'
 
 makedirs(INDEX_PAGES_DIR,exist_ok=True)
 
 for pagenum in range(MAX_PAGE_NUM):
-    resp = requests.get(os.path.join(URL_ENDPOINT +''.format(pagenum)))
+    resp = requests.get(os.path.join(URL_ENDPOINT +str(pagenum)))
     print('Downloading',resp.url)
 
     fname = os.path.join(INDEX_PAGES_DIR,'{}.html'.format(pagenum))
