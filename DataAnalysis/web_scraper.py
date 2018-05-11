212ceb572227455141c3196f6430ecba38515a17
JKChang2015 <jkchang2015@gmail.com>
2018-05-11 13:29:39 +0100

    update

--- DataAnalysis/web_scraper.py
+++ DataAnalysis/web_scraper.py
@@ -5,21 +5,55 @@
 # Description: 
 # http://www.compjour.org/warmups/govt-text-releases/collect-lists-of-obama-press-briefings/
 
+
+
 import requests
 from os import makedirs
 import os
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from glob import glob
 
 URL_ENDPOINT = 'https://www.whitehouse.gov/briefings-statements/page/'
 MAX_PAGE_NUM = 261 #259
 INDEX_PAGES_DIR = 'index-pages'
 
-makedirs(INDEX_PAGES_DIR,exist_ok=True)
+# save the lidex file to local ----------------------------------
+# makedirs(INDEX_PAGES_DIR,exist_ok=True)
+#
+# for pagenum in range(MAX_PAGE_NUM):
+#     resp = requests.get(os.path.join(URL_ENDPOINT +str(pagenum)))
+#     print('Downloading',resp.url)
+#
+#     fname = os.path.join(INDEX_PAGES_DIR,'{}.html'.format(pagenum))
+#     print('Saving',fname)
+#     with open(fname,'w') as wf:
+#         wf.write(resp.text)
 
-for pagenum in range(MAX_PAGE_NUM):
-    resp = requests.get(os.path.join(URL_ENDPOINT +str(pagenum)))
-    print('Downloading',resp.url)
+# extract all the hyperlink -------------------------------------
 
-    fname = os.path.join(INDEX_PAGES_DIR,'{}.html'.format(pagenum))
-    print('Saving',fname)
-    with open(fname,'w') as wf:
-        wf.write(resp.text)
+# WH_BASE_URL = 'https://www.whitehouse.gov/briefings-statements/'
+#
+# links = []
+# ip_fnames = glob(os.path.join(INDEX_PAGES_DIR, '*.html'))
+# for fname in ip_fnames:
+#     with open(fname, 'r') as rf:
+#         txt = rf.read()
+#
+#     soup = BeautifulSoup(txt, 'lxml')
+#     for h in soup.find_all('h2'):
+#         a = h.find('a')
+#         url = urljoin(WH_BASE_URL, a.attrs['href'])
+#         links.append(url)
+#
+# with open('links.txt','w') as wf:
+#     wf.write('\n'.join(links))
+
+# extract the date and the content of the hyperlink--------------------
+links =[]
+links = open('links.txt','r').read().split('\n')
+print(links)
+
+
+
+
