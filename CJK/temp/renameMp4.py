82e04ca8d5e3ab586e02defe3b573b78a6b27be3
JKChang2015 <jkchang2015@gmail.com>
2018-07-10 11:16:25 +0100

    rename videos

--- CJK/temp/renameMp4.py
+++ CJK/temp/renameMp4.py
@@ -8,9 +8,9 @@ import os
 import re
 
 # path = r'/Users/jkchang/Desktop/test'
-path = r'/Users/jkchang/Desktop/untitled folder'
+path = r'/Users/jkchang/Desktop/up'
 
-fileCount = 1
+fileCount = 51
 
 sublist = sorted(os.listdir(path))
 try:
@@ -18,18 +18,27 @@ try:
 except:
     pass
 
+print(sublist)
+
+
+def semi_num(text):
+    return int(re.match(r'(\d+).(\d+)', text).group(2))
+
 for sub in sublist:
 
     subpath = os.path.join(path, sub)
-    txtlist = sorted(os.listdir(subpath))
+    txtlist = os.listdir(subpath)
     try:
         txtlist.remove('.DS_Store')
     except:
         pass
 
+    txtlist.sort(key=semi_num)
+
     for txtfile in txtlist:
-        file_removed = re.sub(r'\d\.\d', '', txtfile)
+        file_removed = re.sub(r'\d\.\d+', '', txtfile)
         sub_removed = re.sub(r'\d*-', '', sub)
         os.rename(os.path.join(subpath, txtfile),
                   os.path.join(subpath, str(fileCount).zfill(3) + " " + sub_removed + " " + file_removed))
         fileCount += 1
+
