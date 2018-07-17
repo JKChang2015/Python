ba4557fe1258da9795f611c9ee0ee2f520fb4000
JKChang2015 <jkchang2015@gmail.com>
2018-07-17 14:08:09 +0100

    extract relationships

--- Work/test/t.py
+++ Work/test/t.py
@@ -8,15 +8,10 @@ from owlready2 import *
 
 from Work.ontology_info import *
 
-onto = get_ontology('file:///Users/jkchang/Downloads/ncit.owl').load()
+onto = get_ontology('file:///Users/jkchang/Downloads/demo.owl').load()
 f = information(onto)
 # print(f.get_supers('pink lady'))
 # print(f.super_count('pink lady'))
 print(f.get_subs('Investigator'))
 # print(f.sub_count('http://purl.enanomapper.org/onto/ENM_9000014'))
 # print(f.get_iri('pink lady'))
-
-
-a = ['cat', 'dog', 'fish']
-response = [{'animal':x} for x in a]
-print(response)
