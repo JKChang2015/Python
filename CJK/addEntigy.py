c4b02f33713bf0ad9f3a6b7481c9ccff352be8b1
JKChang2015 <jkchang2015@gmail.com>
2018-09-10 13:02:18 +0100

    add entity

---
+++ CJK/addEntigy.py
@@ -0,0 +1,19 @@
+# addEntigy
+# Created by JKChang
+# 10/09/2018, 11:39
+# Tag:
+# Description: 
+
+temp = """
+
+
+    <!-- http://www.ebi.ac.uk/metabolights/ontology/MTBLS_000007 -->
+
+    <owl:Class rdf:about="http://www.ebi.ac.uk/metabolights/ontology/MTBLS_000007">
+        <rdfs:subClassOf rdf:resource="http://www.ebi.ac.uk/metabolights/ontology/MTBLS_000006"/>
+        <rdfs:label xml:lang="en">ADW:Commensal species</rdfs:label>
+    </owl:Class>
+    """
+
+
+
