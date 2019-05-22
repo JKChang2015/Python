87612e5803b6eff85d2678fa9e3020064f9c654e
JKChang2015 <jkchang2015@gmail.com>
2019-05-22 14:34:47 +0100

    method to add new term to metabolights ontology

--- Work/ontology/add_entity.py
+++ Work/ontology/add_entity.py
@@ -1,65 +1,66 @@
 # add_entity
 # Created by JKChang
-# 2019-03-12, 10:56
-# Tag:
-# Description: add new class from the spread sheet
+# 2019-05-22, 10:56
+# Description: add new class to ontology and save
 
 
-from owlready2 import get_ontology
 import types
 
-label = 'kiwi'
-definition = 'kind of green fruit'
-superclass = 'fruit'
-
-onto = get_ontology('./resources/metabolights.owl').load()
-
-# onto = get_ontology('./resources/plant.owl').load()
-# onto = get_ontology('../resources/Metabolights.owl').load()
-
-namespace = onto.get_namespace('http://www.ebi.ac.uk/metabolights/ontology/')
-
-with namespace:
-    cls = onto.search_one(label='role')
-
-    NewClass = types.new_class("MTBLS_233", (cls,))
-    print(type(NewClass))
+from owlready2 import get_ontology
 
 
-    # class MTBLS1001(cls):
-    #     pass
-    #
-    #
-    # print(type(MTBLS1001))
-    # MTBLS1001.label = label
-    # MTBLS1001.isDefinedBy = definition
+def addEntity(ontoPath, new_term, supclass, definition=None):
+    '''
+    add new term to the ontology and save it
 
-for c in onto.classes():
-    print(c)
+    :param ontoPath: Ontology Path
+    :param new_term: new entity to be added
+    :param supclass:  superclass/branch name or iri of new term
+    :param definition (optional): definition of the new term
+    '''
+    try:
+        onto = get_ontology(ontoPath).load()
+        id = getid(onto)
+        namespace = onto.get_namespace('http://www.ebi.ac.uk/metabolights/ontology/')
 
-# with onto:
-#     cls = onto.search_one(label = 'apple')
-#     d = cls.isDefinedBy
-#     print(d)
-#
-#
-# with onto:
-#     cls = onto.search_one(label=superclass)
-#
-#     class MTBLS100(cls):
-#         pass
-#     print(type(MTBLS100))
-#     MTBLS100.label = label
-#     MTBLS100.isDefinedBy = definition
+        with namespace:
+            try:
+                cls = onto.search_one(label=supclass)
+            except:
+                try:
+                    cls = onto.search_one(iri=supclass)
+                except Exception as e:
+                    print(e)
+
+            newEntity = types.new_class(id, (cls,))
+            newEntity.label = new_term
+            if definition != None:
+                newEntity.isDefinedBy = definition
+            else:
+                pass
+
+        onto.save(file=ontoPath, format='rdfxml')
+
+    except Exception as e:
+        print(e)
 
 
-onto.save(file='fruit added.owl', format='rdfxml')
+def getid(onto):
+    '''
+    this method usd for get the last un-take continuously term ID
+    :param onto: ontology
+    :return: the last id for the new term
+    '''
 
-# print(cls.isDefinedBy)
-# # print(onto.base_iri)
-# # print(onto.classes())
-# # pro = list(onto.properties())
-# # print(pro)
+    temp = []
+    for c in onto.classes():
+        if str(c).startswith('metabolights'):
+            temp.append(str(c))
 
+    last = max(temp)
+    temp = str(int(last[-6:]) + 1).zfill(6)
+    id = 'MTBLS_' + temp
 
-print()
+    return id
+
+# addEntity('./resources/metabolights.owl', 'papapa', 'role', 'papap def')
