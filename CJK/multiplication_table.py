1283d8eaf8e306239a309dd03c32669c4c74bcb0
JKChang2015 <jkchang2015@gmail.com>
2019-11-18 14:57:43 +0000

    t

---
+++ CJK/multiplication_table.py
@@ -0,0 +1,14 @@
+# multiplication_table
+# Created by JKChang
+# 18/11/2019, 13:07
+# Tag:
+# Description: 
+
+# for i in range(1,10):
+#     for j in range(1,10):
+#         if j <= i:
+#             print(f'{j} x {i} = ' + str(i*j) , end='\t')
+#     print()
+
+
+print('\n'.join('\t'.join([f'{j} x {i} = ' + str(j * i) for j in range(1, i + 1)]) for i in range(1, 10)))
