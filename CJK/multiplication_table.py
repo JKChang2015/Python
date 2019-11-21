d9312331577e9bee46431564a23e057156460138
JKChang2015 <jkchang2015@gmail.com>
2019-11-21 14:51:10 +0000

    multiplication table

--- CJK/multiplication_table.py
+++ CJK/multiplication_table.py
@@ -4,11 +4,10 @@
 # Tag:
 # Description: 
 
-# for i in range(1,10):
-#     for j in range(1,10):
-#         if j <= i:
-#             print(f'{j} x {i} = ' + str(i*j) , end='\t')
-#     print()
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print(f'{j} x {i} = ' + str(i * j), end='\t')
+    print()
 
 
 print('\n'.join('\t'.join([f'{j} x {i} = ' + str(j * i) for j in range(1, i + 1)]) for i in range(1, 10)))
